11cdadedb822d93fd41ca7c40aac4e34b535cea8
Vyacheslav Egorov <tjs05@mail.ru>
2020-04-26 17:20:51 +0300

    Version 1

---
+++ app/lots.py
@@ -0,0 +1,84 @@
+import requests
+import json
+import time
+import random
+
+def createLot(phone, token, amount, volume, emoji_list, show_name_list, params):
+
+    headers = {"Authorization": "Bearer {}".format(token)}
+    data = {'volume': {'value': str(volume), 'uom': params['uom']}, 'cost':{'amount': str(amount), 'currency': 'rub'}, 'trafficType': params['trafficType']}
+
+    print('Creating new voice lot: volume {}, amount {}'.format(volume, amount), end ='   ')
+
+    r = requests.put("https://msk.tele2.ru/api/subscribers/{}/exchange/lots/created".format(phone), headers=headers, data=json.dumps(data))
+
+    print(r.status_code, end=" ")
+
+    response = json.loads(r.text)
+
+    if r.status_code != 200:
+        print("Cannot create new lot: ", response)
+        exit(0)
+
+    time.sleep(1)
+
+    id = response['data']['id']
+    emoji = [random.choice(emoji_list), random.choice(emoji_list), random.choice(emoji_list)]
+
+    #add name and emoji
+    data = {'showSellerName': random.choice(show_name_list), 'emojis': [emoji[0],emoji[1], emoji[2]],'cost':{'amount':str(amount), 'currency': 'rub'}}
+
+    print(' '.join(s for s in emoji))
+
+    r = requests.patch("https://msk.tele2.ru/api/subscribers/{}/exchange/lots/created/{}".format(phone, id), headers=headers, data=json.dumps(data))
+
+
+def deleteLot(phone, token, volume, amount, id, uom, trafficType):
+
+    headers = {"Authorization": "Bearer {}".format(token)}
+    data = {'volume': {'value': str(volume), 'uom': uom}, 'cost': {'amount': str(amount), 'currency': 'rub'},
+            'trafficType': trafficType}
+
+    print('Deleting lot id: volume {}, amount: {}, id: {}'.format(volume, amount, id), end ='  ')
+
+    r = requests.delete("https://msk.tele2.ru/api/subscribers/{}/exchange/lots/created/{}".format(phone, id), headers=headers,
+                     data=json.dumps(data))
+
+    response = json.loads(r.text)
+
+    time.sleep(1)
+
+    print(str(r.status_code), response['meta']['status'])
+
+
+def getLots(phone, token):
+
+    headers = {"Authorization": "Bearer {}".format(token)}
+
+    print('Fetching current lots..')
+
+    r = requests.get("https://msk.tele2.ru/api/subscribers/{}/exchange/lots/created".format(phone), headers=headers)
+
+    if r.status_code == 200:
+        created_lot = json.loads(r.text)
+    else:
+        print(r.status_code)
+        exit(0)
+
+    current_lots = []
+
+    for data in created_lot['data']:
+        if data['status'] == 'active':
+            current_lots.append({'id': data['id'], 'volume': data['volume']['value'], 'trafficType': data['trafficType'], 'uom': data['volume']['uom'], 'amount': data['cost']['amount']})
+
+    return current_lots
+
+def deleteCurrentLots(phone, token):
+
+    current_lots = getLots(phone, token)
+
+    print('Found {} lot(s)'.format(str(len(current_lots))))
+
+    if len(current_lots) > 0:
+        for lot in current_lots:
+            deleteLot(phone, token, lot['volume'], lot['amount'], lot['id'], lot['uom'], lot['trafficType'])
---
+++ app/trade.py
@@ -0,0 +1,55 @@
+import auth
+import lots
+import time
+import random
+import math
+import os
+
+default_phone = os.getenv('default_phone') or ''
+default_token = os.getenv('default_token') or ''
+
+data_type = "voice" #voice or data
+
+# список emoji
+emoji_list = ["bomb", "cat", "cool","devil", "rich", "scream","tongue", "zipped"]
+
+# список для показа имени
+show_name_list = ["true", "false"]
+# настроечные параметры для data и voice
+params = {'voice': {'uom': 'min', 'trafficType': 'voice'}, 'data': {'uom': 'gb', 'trafficType': 'data'}}
+
+# какими пакетами продавать // для voice можно вставтить random.randint(6, 9) * 10
+price = {'voice': {'volume': 50}, 'data': {'volume': 1}}
+# сколько пакетов продавать за раз
+
+count = 7
+
+#получение номера телефона и токена
+phone, token = auth.getToken(default_phone, default_token)
+
+print('Trading is trarted')
+print('Trade: ', phone)
+
+def main():
+    while True:
+
+        #удаление всех лотов
+        lots.deleteCurrentLots(phone, token)
+
+        volume = price[data_type]['volume']
+        amount = math.ceil(volume * 0.8) if data_type == 'voice' else math.ceil(volume * 15)
+
+        for _ in range(count):
+            lots.createLot(phone, token, amount, volume, emoji_list, show_name_list, params[data_type])
+
+        # ожидание
+        wait_time = random.randint(180,250)
+
+        print("Waiting {} seconds".format(str(wait_time)))
+
+        time.sleep(wait_time)
+
+
+
+if __name__ == '__main__':
+    main()
