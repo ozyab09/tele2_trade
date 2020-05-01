4a7bcbfeeb8ad19282088d9e9453eccc965dd54b
Anton Dmitrievsky <admitrievsky@gmail.com>
2020-05-01 18:00:34 +0500

    Do refactoring

--- app/client.py
+++ app/client.py
@@ -1,3 +1,5 @@
+import random
+
 import requests
 
 
@@ -5,15 +7,18 @@ class Tele2Client:
     BASE_URL = 'https://msk.tele2.ru'
     AUTH_URL = f'{BASE_URL}/auth'
     API_URL = f'{BASE_URL}/api'
-    LOTS_CREATED_URL = BASE_URL + (
+    LOTS_URL = BASE_URL + (
         '/https://msk.tele2.ru/api/subscribers/{phone}/exchange/lots/created')
+    LOT_URL = LOTS_URL + '/{lot_id}'
 
     METHODS = {
         'send-otp': ('post', API_URL + '/validation/number/{phone}'),
         'get-token-from-otp': (
             'post',
             AUTH_URL + '/auth/realms/tele2-b2c/protocol/openid-connect/token'),
-        'create-lot': ('put', LOTS_CREATED_URL)
+        'create-lot': ('put', LOTS_URL),
+        'patch-lot': ('patch', LOT_URL),
+        'delete-lot': ('delete', LOT_URL)
     }
 
     def _call(self, method_name, token=None, url_args=None, payload=None):
@@ -50,7 +55,27 @@ class Tele2Client:
         data = {'volume': {'value': str(volume), 'uom': uom,
                            'cost': {'amount': str(amount), 'currency': 'rub'},
                            'trafficType': traffic_type}}
-        return self._call('create-lot', token=token, url_args={'phone': phone},
+        response = self._call('create-lot', token=token,
+                              url_args={'phone': phone},
+                              payload=data)
+        return response['data']
+
+    def patch_lot(self, phone, token, lot_id, show_name_list, emojis, amount):
+        data = {'showSellerName': random.choice(show_name_list),
+                'emojis': emojis,
+                'cost': {'amount': str(amount), 'currency': 'rub'}}
+        response = self._call('patch-lot', token=token,
+                              url_args={'phone': phone, 'lot_id': lot_id},
+                              payload=data)
+        return response
+
+    def delete_lot(self, phone, token, lot_id, volume, uom, amount,
+                   traffic_type):
+        data = {'volume': {'value': str(volume), 'uom': uom,
+                           'cost': {'amount': str(amount), 'currency': 'rub'},
+                           'trafficType': traffic_type}}
+        return self._call('delete-lot', token=token,
+                          url_args={'phone': phone, 'lot_id': lot_id},
                           payload=data)
 
 
--- app/lots.py
+++ app/lots.py
@@ -20,40 +20,31 @@ def create_lot(phone, token, amount, volume, emoji_list, show_name_list,
 
     time.sleep(1)
 
-    id = lot['data']['id']
-    emoji = [random.choice(emoji_list), random.choice(emoji_list),
-             random.choice(emoji_list)]
+    # add name and emojis
+    lot_id = lot['id']
+    emojis = random.choices(emoji_list, k=3)
+    try:
+        tele2_client.patch_lot(
+            phone=phone, token=token, lot_id=lot_id,
+            show_name_list=show_name_list,
+            emojis=emojis, amount=amount)
+    except requests.RequestException:
+        print('Unable to update the lot')
+        exit()
 
-    # add name and emoji
-    data = {'showSellerName': random.choice(show_name_list),
-            'emojis': [emoji[0], emoji[1], emoji[2]],
-            'cost': {'amount': str(amount), 'currency': 'rub'}}
 
-    print(' '.join(s for s in emoji))
-
-    r = requests.patch("https://msk.tele2.ru/api/subscribers/{}/exchange/lots/created/{}".format(phone, id), headers=headers, data=json.dumps(data))
-
-
-def delete_lot(phone, token, volume, amount, id, uom, trafficType):
-    headers = {"Authorization": "Bearer {}".format(token)}
-    data = {'volume': {'value': str(volume), 'uom': uom},
-            'cost': {'amount': str(amount), 'currency': 'rub'},
-            'trafficType': trafficType}
-
-    print(
-        'Deleting lot id: volume {}, amount: {}, id: {}'.format(volume, amount,
-                                                                id), end='  ')
-
-    r = requests.delete(
-        "https://msk.tele2.ru/api/subscribers/{}/exchange/lots/created/{}".format(
-            phone, id), headers=headers,
-        data=json.dumps(data))
-
-    response = json.loads(r.text)
+def delete_lot(phone, token, volume, amount, lot_id, uom, traffic_type):
+    print(f'Deleting lot id: volume {volume}, amount: {amount}, id: {lot_id}',
+          end='  ')
+    try:
+        tele2_client.delete_lot(
+            phone=phone, token=token, lot_id=lot_id, volume=volume, uom=uom,
+            amount=amount, traffic_type=traffic_type)
+    except requests.RequestException:
+        print('Unable to delete the lot')
+        exit()
 
     time.sleep(1)
-
-    print(str(r.status_code), response['meta']['status'])
 
 
 def get_lots(phone, token):
